6eae7f64e22b6cabaf71ef7e0dfe042c1e2ae50b
lucasviieira <lucasviieira_@hotmail.com>
2015-08-06 14:40:30 -0700

    Versao 1.2
    Bugs Arrumados

--- trabalho_relacoes_entre_retas_GA.c
+++ trabalho_relacoes_entre_retas_GA.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int vetorDiretor1[3];
-int vetorDiretor2[3];
-int ponto1[3];
-int ponto2[3];
-int vetorDiretor12[3];
-int pontoInter[3];
+float vetorDiretor1[3];
+float vetorDiretor2[3];
+float ponto1[3];
+float ponto2[3];
+float vetorDiretor12[3];
+float pontoInter[3];
 
 void recebeDados();
 void mostraDados();
 void menu();
-void verificaRetas(int *vetorDiretor1, int *vetorDiretor2, int *vetorDiretor12, int *ponto1, int *ponto2);
-int retasReversas(int *vetorDiretor1, int *vetorDiretor2, int *vetorDiretor12);
-int retasOrtogonais(int *vetorDiretor1, int *vetorDiretor2);
-int retasParalelas(int *vetorDiretor1, int *vetorDiretor2);
-int retasCoincidentes(int *ponto2, int *vetorDiretor1, int *ponto1);
+void pontoIntersecao(float *vetorDiretor1, float *vetorDiretor2, float *ponto1, float *ponto2);
+void verificaRetas(float *vetorDiretor1, float *vetorDiretor2, float *vetorDiretor12, float *ponto1, float *ponto2);
+int retasReversas(float *vetorDiretor1, float *vetorDiretor2, float *vetorDiretor12);
+int retasOrtogonais(float *vetorDiretor1, float *vetorDiretor2);
+int retasParalelas(float *vetorDiretor1, float *vetorDiretor2);
+int retasCoincidentes(float *ponto2, float *vetorDiretor1, float *ponto1);
 
 void recebeDados(){
 
@@ -23,35 +24,35 @@ void recebeDados(){
     printf("Um reta passa por um ponto A(x,y,z) e possui um vetor diretor(a,b,c).\n");
     //recebe as coordenas do ponto em que a reta 1 passa ponto1(X1,Y1,Z1)
     printf("\nDigite a coordenada em X do ponto em que a reta 1 passa.\n");
-    scanf("%d", &ponto1[0]);
+    scanf("%f", &ponto1[0]);
     printf("Digite a coordenada em Y do ponto em que a reta 1 passa.\n");
-    scanf("%d", &ponto1[1]);
+    scanf("%f", &ponto1[1]);
     printf("Digite a coordenada em Z do ponto em que a reta 1 passa.\n");
-    scanf("%d", &ponto1[2]);
+    scanf("%f", &ponto1[2]);
 
     //recebe as coordenas do ponto em que a reta 1 passa ponto2(X2,Y2,Z2)
     printf("\nDigite a coordenada em X do ponto em que a reta 2 passa.\n");
-    scanf("%d", &ponto2[0]);
+    scanf("%f", &ponto2[0]);
     printf("Digite a coordenada em Y do ponto em que a reta 2 passa.\n");
-    scanf("%d", &ponto2[1]);
+    scanf("%f", &ponto2[1]);
     printf("Digite a coordenada em Z do ponto em que a reta 2 passa.\n");
-    scanf("%d", &ponto2[2]);
+    scanf("%f", &ponto2[2]);
 
     //recebe as coordenas do vetor diretor da reta 1(a1,b1,c1)
     printf("\nDigite a coordenada em X do vetor diretor da reta1.\n");
-    scanf("%d", &vetorDiretor1[0]);
+    scanf("%f", &vetorDiretor1[0]);
     printf("Digite a coordenada em Y do vetor diretor da reta1.\n");
-    scanf("%d", &vetorDiretor1[1]);
+    scanf("%f", &vetorDiretor1[1]);
     printf("Digite a coordenada em Z do vetor diretor da reta1.\n");
-    scanf("%d", &vetorDiretor1[2]);
+    scanf("%f", &vetorDiretor1[2]);
 
     //recebe as coordenas do vetor diretor da reta 2(a2,b2,c2)
     printf("\nDigite a coordenada em X do vetor diretor da reta2.\n");
-    scanf("%d", &vetorDiretor2[0]);
+    scanf("%f", &vetorDiretor2[0]);
     printf("Digite a coordenada em Y do vetor diretor da reta2.\n");
-    scanf("%d", &vetorDiretor2[1]);
+    scanf("%f", &vetorDiretor2[1]);
     printf("Digite a coordenada em Z do vetor diretor da reta2.\n");
-    scanf("%d", &vetorDiretor2[2]);
+    scanf("%f", &vetorDiretor2[2]);
 
     vetorDiretor12[0] = (ponto2[0] - ponto1[0]);
     vetorDiretor12[1] = (ponto2[1] - ponto1[1]);
@@ -76,44 +77,44 @@ system("cls");
                 if(i == 0){
                     printf("Ponto 1 = ");
                 }
-                printf("[%d] ", ponto1[i]);
+                printf("[%.1f] ", ponto1[i]);
             }
             if(contador == 1){
             // mostra as coordernadas do ponto 2
                 if(i == 0){
                     printf("Ponto 2 = ");
                 }
-                printf("[%d] ", ponto2[i]);
+                printf("[%.1f] ", ponto2[i]);
             }
             if(contador == 2){
              // mostra as coordernadas do vetor diretor 1
                 if(i == 0){
                     printf("Vetor Diretor 1 = ");
                 }
-                printf("[%d] ", vetorDiretor1[i]);
+                printf("[%.1f] ", vetorDiretor1[i]);
             }
             if(contador == 3){
             // mostra as coordernadas do vetor diretor 2
                 if(i == 0){
                     printf("Vetor Diretor 2 = ");
                 }
-                printf("[%d] ", vetorDiretor2[i]);
+                printf("[%.1f] ", vetorDiretor2[i]);
             }
             if(contador == 4){
             // mostra as coordernadas do vetor diretor 12
                 if(i == 0){
                     printf("Vetor Diretor 12 = ");
                 }
-                printf("[%d] ", vetorDiretor12[i]);
+                printf("[%.1f] ", vetorDiretor12[i]);
             }
         } printf("\n");
         contador++;
     } printf("\n");
 }
 
-int retasReversas(int *vetorDiretor1, int *vetorDiretor2, int *vetorDiretor12){
+int retasReversas(float *vetorDiretor1, float *vetorDiretor2, float *vetorDiretor12){
 // fazendo o determinante entre os vetores diretores eh possivel determinar se as retas sao reversas ou nao.
-int determinante = 0;
+float determinante = 0.0;
 determinante+= (vetorDiretor1[0] * vetorDiretor2[1] * vetorDiretor12[2]);
 determinante+= (vetorDiretor1[1] * vetorDiretor2[2] * vetorDiretor12[0]);
 determinante+= (vetorDiretor1[2] * vetorDiretor2[0] * vetorDiretor12[1]);
@@ -123,14 +124,15 @@ determinante+= (vetorDiretor1[1] * vetorDiretor2[0] * vetorDiretor12[2])*-1;
 
     //printf("\nO determinante eh: %d \n", determinante);
 // se o determinante der 0 os vetores diretores sao coplanares e portanto as retas nao sao reversas
-    if(determinante == 0) return 0;
+    if(determinante == 0.0) return 0;
 // se o determinante der diferente de 0 os vetores diretores nao sao coplanares e portanto as retas sao reversas
     else return 1;
 }
 
-int retasOrtogonais(int *vetorDiretor1, int *vetorDiretor2){
+int retasOrtogonais(float *vetorDiretor1, float *vetorDiretor2){
 
-int resultado = 0, i = 0;
+float resultado = 0;
+int i = 0;
     // se o produto vetorial entre os vetores diretores for 0, as retas sao ortogonais.
     for(i = 0; i < 3; i++){
         resultado+= vetorDiretor1[i] * vetorDiretor2[i];
@@ -139,22 +141,22 @@ int resultado = 0, i = 0;
     else return 0;
 }
 
-int retasParalelas(int *vetorDiretor1, int *vetorDiretor2){
+int retasParalelas(float *vetorDiretor1, float *vetorDiretor2){
 
 int erro[3] = {4,4,4};
-int qualErro[3];
-int k[3], i = 0;
+float k[3];
+int i = 0;
 
     // se existir um K que seja possivel escrever vetorDiretor1 = k*vetorDiretor2, entao as retas sao paralelas
     for(i = 0; i < 3; i++){
-        // testa se as coordenadas são 0
-        if(vetorDiretor1[i] == 0 && vetorDiretor2[i] == 0){
+        // testa se as coordenadas sao 0
+        if(vetorDiretor1[i] == 0.0 && vetorDiretor2[i] == 0.0){
             erro[i] = 1;
         }
-        else if(vetorDiretor1[i] == 0){
+        else if(vetorDiretor1[i] == 0.0){
             k[i] == 158;
         }
-        else if(vetorDiretor2[i] == 0){
+        else if(vetorDiretor2[i] == 0.0){
             k[i] == 159;
         }
         else{
@@ -186,9 +188,9 @@ int k[3], i = 0;
     else return 0;
 }
 
-int retasCoincidentes(int *ponto1, int *vetorDiretor1, int *ponto2){
+int retasCoincidentes(float *ponto1, float *vetorDiretor1, float *ponto2){
 
-int lambda[3] = {99,99,99};
+float lambda[3] = {99,99,99};
 int erro[3] = {4,4,4};
 int erros = 0;
 int ignore[3] = {0,0,0};
@@ -250,29 +252,114 @@ int i = 0;
 
 }
 
-void pontoIntersecao(int *vetorDiretor1, int *vetorDiretor2, int *ponto1, int *ponto2){
+void pontoIntersecao(float *vetorDiretor1, float *vetorDiretor2, float *ponto1, float *ponto2){
 
-int lambda = 0;
-int tempomi1 = 0;
-int tempomi2 = 0;
-int tempolambda1 = 0;
-int tempolambda2 = 0;
-int mi = 0;
-int X = 1;
-int Y = 0;
-int Z = 0;
+float lambda = 0;
+float tempomi1 = 0;
+float tempomi2 = 0;
+float tempolambda1 = 0;
+float tempolambda2 = 0;
+float mi = 0;
+float teste = 0;
+int incoginata = 0;
 
     //o ponto eh determinando igualando as equacoes parametricas das retas
-    tempomi1 = (ponto1[0] + (-1*ponto2[0]))/vetorDiretor2[0];
-    tempomi2 = (ponto1[1] + (-1*ponto2[1]))/vetorDiretor2[1];
-    tempolambda1 = (tempomi1 + (-1*tempomi2));
-    tempolambda2 = (vetorDiretor1[1]/vetorDiretor2[1]) + (-1*(vetorDiretor1[0]/vetorDiretor2[0]));
-    lambda = tempolambda1/tempolambda2;
+    //caso haja duas posicoes iguais dos vetores diretores com 0, sera testado as outras duas posicoes
+    if(vetorDiretor1[0] == 0 && vetorDiretor2[0] == 0){
+        tempomi1 = (ponto1[1] - ponto2[1])/vetorDiretor2[1];
+
+        tempomi2 = (ponto1[2] - ponto2[2]);
+        tempomi2 = tempomi2 / vetorDiretor2[2];
+
+        tempolambda1 = (tempomi1 - tempomi2);
+
+        tempolambda2 = (vetorDiretor1[2]/vetorDiretor2[2]) + (-1*(vetorDiretor1[1]/vetorDiretor2[1]));
+
+        lambda = tempolambda1/tempolambda2;
+
+        incoginata = 1;
+    }
+    else if(vetorDiretor1[1] == 0 && vetorDiretor2[1] == 0){
+        tempomi1 = (ponto1[0] - ponto2[0])/vetorDiretor2[0];
+
+        tempomi2 = (ponto1[2] - ponto2[2]);
+        tempomi2 = tempomi2 / vetorDiretor2[2];
+
+        tempolambda1 = (tempomi1 - tempomi2);
+
+        tempolambda2 = (vetorDiretor1[2]/vetorDiretor2[2]) + (-1*(vetorDiretor1[0]/vetorDiretor2[0]));
+
+        lambda = tempolambda1/tempolambda2;
+
+        incoginata = 1;
+    }
+    else if(vetorDiretor1[2] == 0 && vetorDiretor2[2] == 0){
+        tempomi1 = (ponto1[0] - ponto2[0])/vetorDiretor2[0];
+
+        tempomi2 = (ponto1[1] - ponto2[1]);
+        tempomi2 = tempomi2 / vetorDiretor2[1];
+
+        tempolambda1 = (tempomi1 - tempomi2);
+
+        tempolambda2 = (vetorDiretor1[1]/vetorDiretor2[1]) + (-1*(vetorDiretor1[0]/vetorDiretor2[0]));
+
+        lambda = tempolambda1/tempolambda2;
+
+        incoginata = 1;
+    }
+    else{
+        //caso haja alguma posicao do vetor diretor com zero, eh possivel descobrir o lambda direto
+        if(vetorDiretor2[0] == 0){
+            lambda = (ponto2[0] - ponto1[0])/vetorDiretor1[0];
+            incoginata = 1;
+        }
+        else if(vetorDiretor2[1] == 0){
+            lambda = (ponto2[1] - ponto1[1])/vetorDiretor1[1];
+            incoginata = 1;
+        }
+        else if(vetorDiretor2[2] == 0){
+            lambda = (ponto2[2] - ponto1[2])/vetorDiretor1[2];
+            incoginata = 1;
+        }
+        //caso haja alguma posicao do vetor diretor com zero, eh possivel descobrir o mi direto
+        else if(vetorDiretor1[0] == 0){
+            mi = (ponto1[0] - ponto2[0])/vetorDiretor2[0];
+        }
+        else if(vetorDiretor1[1] == 0){
+            mi = (ponto1[1] - ponto2[1])/vetorDiretor2[1];
+        }
+        else if(vetorDiretor1[2] == 0){
+            mi = (ponto1[2] - ponto2[2])/vetorDiretor2[2];
+        }
+
+        else{
+            tempomi1 = (ponto1[0] - ponto2[0])/vetorDiretor2[0];
+
+            tempomi2 = (ponto1[1] - ponto2[1]);
+            tempomi2 = tempomi2 / vetorDiretor2[1];
+
+            tempolambda1 = (tempomi1 - tempomi2);
+
+            tempolambda2 = (vetorDiretor1[1]/vetorDiretor2[1]) + (-1*(vetorDiretor1[0]/vetorDiretor2[0]));
+
+            lambda = tempolambda1/tempolambda2;
+
+            incoginata = 1;
+        }
+    }
+
     //mi = ((ponto1[0] + (-1*ponto2[0]))/vetorDiretor2[0]) + ((vetorDiretor1[0]/vetorDiretor2[0])*lambda);
 
-    pontoInter[0] = ponto1[0] + (vetorDiretor1[0]*lambda);
-    pontoInter[1] = ponto1[1] + (vetorDiretor1[1]*lambda);
-    pontoInter[2] = ponto1[2] + (vetorDiretor1[2]*lambda);
+    if(incoginata == 1){
+        pontoInter[0] = ponto1[0] + (vetorDiretor1[0]*lambda);
+        pontoInter[1] = ponto1[1] + (vetorDiretor1[1]*lambda);
+        pontoInter[2] = ponto1[2] + (vetorDiretor1[2]*lambda);
+    }
+    else{
+        pontoInter[0] = ponto2[0] + (vetorDiretor2[0]*mi);
+        pontoInter[1] = ponto2[1] + (vetorDiretor2[1]*mi);
+        pontoInter[2] = ponto2[2] + (vetorDiretor2[2]*mi);
+    }
 
 }
 
@@ -305,7 +392,7 @@ int escolha = 0;
     }
     else if(escolha == 3){
         system("cls");
-        printf("Este programa foi desenvolvido para a disciplina de Geometria Analitica e       Calculo Vetorial.\n");
+        printf("Este programa foi desenvolvido para a disciplina de Geometria Analitica e Calculo Vetorial.\n");
         printf("\nO mesmo foi criado pelos alunos:\n\n- Lucas Leandro\n- Jesse Clemente\n- Ocimar Henrique\n\n");
         system("pause");
         menu();
@@ -320,59 +407,48 @@ int escolha = 0;
     }
 }
 
-void verificaRetas(int *vetorDiretor1, int *vetorDiretor2, int *vetorDiretor12, int *ponto1, int *ponto2){
+void verificaRetas(float *vetorDiretor1, float *vetorDiretor2, float *vetorDiretor12, float *ponto1, float *ponto2){
 
-int vetorTeste1[3] = {0,-1,1};
-int vetorTeste2[3] = {0,2,-2};
-int pontoTeste1[3] = {2,5,-4};
-int pontoTeste2[3] = {2,0,1};
-int vetorTeste3[3] = {0,-5,5};
+float vetorTeste1[3] = {0,-1,1};
+float vetorTeste2[3] = {0,2,-2};
+float pontoTeste1[3] = {2,5,-4};
+float pontoTeste2[3] = {2,0,1};
+float vetorTeste3[3] = {0,-5,5};
+
     // testa se as retas sao reversas:
     if(retasReversas(vetorDiretor1, vetorDiretor2, vetorDiretor12)){
         printf("As retas sao reversas\n");
 
         // caso sejam reversas, testa se alem de reversas, sao ortogonais
         if(retasOrtogonais(vetorDiretor1, vetorDiretor2)){
-            printf("\nAs retas sao ortogonais\n");
+            printf("\nE sao retas ortogonais\n");
         }
-        else{
-            printf("\nAs retas nao sao ortogonais\n");
-        }
-        // testa o ponto de intersecao entre as retas
-        pontoIntersecao(vetorDiretor1, vetorDiretor2, ponto1, ponto2);
-        printf("\nAs Retas se encontram no ponto de coordenadas[%d,%d,%d]\n", pontoInter[0], pontoInter[1], pontoInter[2]);
+
     }
     else{
-        printf("As retas nao sao reversas\n");
-
         // se elas nao forem reversas, testa se sao paralelas
         if(retasParalelas(vetorDiretor1, vetorDiretor2)){
             printf("\nAs retas sao paralelas\n");
 
             // se elas forem paralelas, testa se alem de paralelas, sao as mesmas retas
             if(retasCoincidentes(ponto2,vetorDiretor2,ponto1)){
-                printf("\nEstas sao retas coincidentes\n");
+                printf("\nE sao retas coincidentes\n");
             }
             else{
-                printf("\nNao sao as mesmas retas\n");
+                printf("\nPorem sao retas distintas\n");
             }
         }
         else{
-            printf("\nAs retas nao sao paralelas\n");
-
             //caso elas nao sejam reversas nem paralelas, elas sao retas concorrentes
             printf("\nAs retas sao concorrentes\n");
 
             // testa se alem de concorrentes, elas sao ortogonais
             if(retasOrtogonais(vetorDiretor1, vetorDiretor2)){
-                printf("\nAs retas sao ortogonais\n");
-            }
-            else{
-                printf("\nAs retas nao sao ortogonais\n");
+                printf("\nE sao retas ortogonais\n");
             }
             // testa o ponto de intersecao entre as retas
             pontoIntersecao(vetorDiretor1, vetorDiretor2, ponto1, ponto2);
-            printf("\nAs Retas se encontram no ponto de coordenadas[%d,%d,%d]\n", pontoInter[0], pontoInter[1], pontoInter[2]);
+            printf("\nAs Retas se encontram no ponto de coordenadas [%.1f] [%.1f] [%.1f]\n", pontoInter[0], pontoInter[1], pontoInter[2]);
         }
     }
     printf("\n");
@@ -382,12 +458,13 @@ int vetorTeste3[3] = {0,-5,5};
 
 int main(){
 
-//int v1[3] = {2,3,4};
-//int v2[3] = {1,-1,-2};
-//int p1[3] = {2,0,5};
-//int p2[3] = {5,2,7};
+//float v1[3] = {0,3,5};
+//float v2[3] = {2,-2,6};
+//float p1[3] = {2,3,6};
+//float p2[3] = {1,4,3};
 
     //pontoIntersecao(v1,v2,p1,p2);
+    //printf("%.1f %.1f %.1f", pontoInter[0], pontoInter[1], pontoInter[2]);
     menu();
     return 0;
 }
